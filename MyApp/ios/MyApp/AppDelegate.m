06d03c27ec9b4169df96948ee518aa700c3e6c11
dianchengwangCHN <dianchengwangchn@gmail.com>
2019-01-05 11:59:57 -0500

    Fix build error on IOS

--- MyApp/ios/MyApp/AppDelegate.m
+++ MyApp/ios/MyApp/AppDelegate.m
@@ -10,13 +10,11 @@
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
 #import <ReactNativeNavigation/ReactNativeNavigation.h>
-#import <GoogleMaps/GoogleMaps.h>
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-  [GMSServices provideAPIKey:@"$System.env.GOOGLE_MAP_KEY"]; // add this line using the api key obtained from Google Console
 
   NSURL *jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
   [ReactNativeNavigation bootstrap:jsCodeLocation launchOptions:launchOptions];
